f3ba88fc08e03dbf028e107a618a267f05c572d3
DESKTOP-K9F28LS\Admin <quanghoang8532@gmail.com>
2022-10-26 23:18:43 +0700

    validate tài khoản

--- services/account.js
+++ services/account.js
@@ -1,6 +1,8 @@
 var connection = require("../connection/connect.js")
 var connect = connection.connection
-
+let isValid = true;
+let isDuplicate
+let listErrors = [];
 // lấy tất cả dữ liệu
 const getAllData = (req, res) => {
     try {
@@ -46,13 +48,20 @@ const deleteRecord = (req, res) => {
 // thêm mới
 const insertRecord = (req, res) => {
     try {
-        var values = Object.values(req.body);
-        let procedure = `CALL proc_insertAccount(?,?,?,?,?,?,?)`;
-        connect.query(procedure, values, function(err, data) {
-            if (err)
-                res.status(400).json(err)
-            res.status(201).json(1)
-        });
+        validateData(req.body);
+        if (!isValid) {
+            res.status(400).json(listErrors)
+        } else {
+            var values = Object.values(req.body);
+            let procedure = `CALL proc_insertAccount(?,?,?,?,?,?,?)`;
+            connect.query(procedure, values, function(err, data) {
+                if (err)
+                    res.status(400).json(err)
+                res.status(201).json(1)
+            });
+        }
+        listErrors = [];
+        isValid = true;
     } catch (error) {
         console.log(error);
     }
@@ -72,6 +81,61 @@ const updateRecord = (req, res) => {
         console.log(error);
     }
 }
+
+function checkDuplicateEmail(email) {
+    isDuplicate = false;
+    try {
+        var sql = `select * from account where email = "` + email + `"`;
+        connect.query(sql, function(err, data) {
+            if (data.length != 0) {
+                console.log("có trùng");
+                isDuplicate = true;
+                console.log("Đã thay đổi", isDuplicate);
+            } else {
+                console.log("không trùng");
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function validateData(data) {
+    if (!data.FirstName) {
+        isValid = false;
+        listErrors.push("Tên người dùng không được để trống");
+    }
+    if (!data.LastName) {
+        isValid = false;
+        listErrors.push("Họ đệm người dùng không được để trống");
+    }
+    if (!data.Email) {
+        isValid = false;
+        listErrors.push("Email không được để trống");
+    }
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!filter.test(data.Email)) {
+        isValid = false;
+        listErrors.push("Email không đúng định dạng");
+    }
+    if (!data.Password) {
+        isValid = false;
+        listErrors.push("Mật khẩu không được để trống");
+    }
+    if (!data.Address) {
+        isValid = false;
+        listErrors.push("Địa chỉ không được để trống");
+    }
+    checkDuplicateEmail(data.Email)
+    console.log("Trong hàm check", isDuplicate);
+    if (isDuplicate == true) {
+        isValid = false;
+        listErrors.push("Email đã tồn tại");
+    }
+}
+
+
+
 module.exports = {
     getAllData,
     getByID,
